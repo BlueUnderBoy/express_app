6df31b1758a762f9f7dddb40da560371f2bac70c
Jalen Young <lital5@yahoo.com>
2024-10-02 22:36:39 -0500

    index a met a jour

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const jparser = require('body-parser')
+const jparser = require('body-parser');
 const app = express();
 const port = 3000;
 
@@ -10,6 +10,7 @@ res.send("");
 app.use(jparser.urlencoded({ extended: true }));
 app.use(jparser.json({ extended: true }));
 
+app.use(express.static(path.join(__dirname, 'templates')))
 
 app.get("/", (req, res) => {
     res.json({
@@ -23,6 +24,15 @@ app.get("/", (req, res) => {
 });
 });
 
+app.use((req, res, next) => {
+    next(error(404, "Resource Not Found"));
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: err.message });
+    });
+
 app.listen(port, () => {
-console.log(`Any sound from port: ${port}?`);
+console.log(`Sounds coming from port: ${port}.`);
 });
