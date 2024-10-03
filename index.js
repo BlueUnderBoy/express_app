dfbe856d35594f034c3d52d1a797d84b251ff47b
Jalen Young <lital5@yahoo.com>
2024-10-02 21:23:03 -0500

    troisieme

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const jparser = require('body-parser')
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
 res.send("");
+});
+
+app.use(jparser.urlencoded({ extended: true }));
+app.use(jparser.json({ extended: true }));
+
+
+app.get("/", (req, res) => {
+    res.json({
+        links: [
+            {
+            href: "/api",
+            rel: "api",
+            type: "GET",
+        },
+    ],
+});
 });
 
 app.listen(port, () => {
