f4dae6692843a786b5abe8c20368d699b80b43b3
Jalen Young <lital5@yahoo.com>
2024-10-02 22:45:39 -0500

    les fichiers de data ont met a jour

---
+++ data/fighters.js
@@ -0,0 +1,35 @@
+const fighters = [
+    {
+      id: 1,
+      name: "Max",
+      age: 10
+    },
+    {
+      id: 2,
+      name: "Mikoto",
+      age: 3
+    },
+    {
+      id: 3,
+      name: "Mittens",
+      age: 4
+    },
+    {
+        id: 4,
+        name: "Mappy",
+        age: 4
+      },
+      {
+        id: 5,
+        name: "Grod",
+        age: 7
+      },
+      {
+        id: 6,
+        name: "Bander",
+        age: 6
+      }
+  ];
+  
+  module.exports = fighters;
+  
---
+++ data/wins.js
@@ -0,0 +1,20 @@
+const wins = [
+    {
+      id: 1,
+      fighterId: [1,2],
+      winner: 2
+      },
+    {
+      id: 2,
+      fighterId: [3,4],
+      winner: 3
+    },
+    {
+      id: 3,
+      fighterId: [5,6],
+      winner: 5
+    }
+    ];
+  
+  module.exports = wins;
+  
